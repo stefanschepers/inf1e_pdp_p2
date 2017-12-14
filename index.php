4338af4011735341798f945be196c5476376c0cf
Stefan Schepers <stefan.schepers1@student.stenden.com>
2017-12-14 12:48:59 +0100

    Logo op homepage

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="/styling/stylesheet.css">
 		<meta charset="UTF-8">
-		<title>iDesign</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>iDesign</title>
 	</head>
 	<body>
-	
+		<div id="container">
+			<div id="header">
+				<img src="/img/logo.png">
+			<div>
+		</div>
 	</body>
 </html>	
