1d87cadb6113ae4c7536d79e1800b973db0d151d
Stefan Schepers <stefan.schepers1@student.stenden.com>
2017-12-18 10:22:45 +0100

    Responsive design begin

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
-		<link rel="stylesheet" type="text/css" href="/styling/stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="styling/style.css">
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>iDesign</title>
@@ -9,8 +9,21 @@
 	<body>
 		<div id="container">
 			<div id="header">
-				<img src="/img/logo.png">
+				<img src="img/logo.png" alt="iDesign Logo" class="logo">
 			<div>
+			<div id="menu">
+				Home
+			</div>
+			<div class="left">
+				Inloggen HIER (Placeholder)
+				<p>Email:<br>
+				<input type="text"><br>
+				Wachtwoord:<br>
+				<input type="text"></p>
+				<button type="button">Inloggen</button>
+			</div>
+			<div class="content">
+			</div>	
 		</div>
 	</body>
 </html>	
