6135675123de2ba8d9cbe7bb84cc5848655661ea
KnightmareX <wesley.velderman@student.stenden.com>
2017-12-18 20:24:40 +0100

    login werkend, werkt nu met row limit en checkt of er een record is gevonden.

--- protofolio/login.php
+++ protofolio/login.php
@@ -8,21 +8,26 @@
 
         <form method="post" action="login.php">
             Gebruikersnaam: <input type="text" name="username" size="20" maxlength="20"><br />
-            Wachtwoord: <input type="password" name="wachtwoord" size="20" maxlength="20"><br /><br />
-            <input type="submit" value="Log in!" />
-        </form><br /><br />
+                Wachtwoord: <input type="password" name="wachtwoord" size="20" maxlength="20"><br /><br />
+                    <input type="submit" name="submit" value="Log in!" />
+                    </form>
 
-        <?php
-            include 'dbci.php';
-            $email = $_POST["username"];
-            $pw = $_POST["wachtwoord"];
-            $query = "SELECT Email, Password FROM user WHERE Email='$email' AND Password='$pw' LIMIT 1";
-            $result = mysqli_query($coni, $query);
-            if($result){
-                session_start();
-                $_SESSION["ingelogd"] = true;
-                echo "ingelogd!";
-            }
-        ?>
-    </body>
-</html>
+                    <?php
+                    if (isset($_POST["submit"])) {
+                        include 'dbci.php';
+                        $email = $_POST["username"];
+                        $pw = $_POST["wachtwoord"];
+                        $query = "SELECT Email, Password FROM user WHERE Email='$email' AND Password='$pw' LIMIT 1";
+                        echo $query;
+                        $result = mysqli_query($coni, $query);
+                        if (mysqli_num_rows($result) == 1) {
+                            session_start();
+                            $_SESSION["ingelogd"] = true;
+                            echo "ingelogd!";
+                        } else {
+                            echo "<br/>Foutieve informatie";
+                        }
+                    }
+                    ?>
+                    </body>
+                    </html>
