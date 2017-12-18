62f66df61e532e097d6a57b7a2c1c04e7e4155df
pascalklinge <p.b.klinge@hotmail.nl>
2017-12-18 12:50:22 +0100

    Inlog.php Wesley gaat deze veder maken

---
+++ protofolio/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="nl" xml:lang="nl">
+    <head>
+        <title>Loginsysteem</title>
+    </head>
+    <body>
+
+        <form method="post" action="check.php">
+            Gebruikersnaam: <input type="text" name="username" size="20" maxlength="20"><br />
+            Wachtwoord: <input type="password" name="wachtwoord" size="20" maxlength="20"><br /><br />
+            <input type="submit" value="Log in!" />
+        </form><br /><br />
+
+        <?php
+        
+        
+        ?>
+    </body>
+</html>
