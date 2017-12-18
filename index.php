8a6b30ef065ba0867a9cd1794d6adcc3a7aab849
Stefan Schepers <stefan.schepers1@student.stenden.com>
2017-12-18 13:05:50 +0100

    Menu aangepast, werkende link naar CMS.

--- index.php
+++ index.php
@@ -13,28 +13,28 @@
             ?>
             
 		
-			<div id="header">
-                            <img src="img/logo.png" alt="iDesign Logo" class="logo"> 
-                        </div>
-                        <div id="menu">
-                            <span class="menufooteritems">Home - Mijn portfolio's - Contact -  CMS</span>
-			</div>
+            <div id="header">
+                <img src="img/logo.png" alt="iDesign Logo" class="logo"> 
+            </div>
+            <div id="menu">
+                <span class="menufooteritems">Home  -  Mijn portfolio's  -  Contact  -  <a href="cms.php">CMS</a></span>
+            </div>
             <div id="container">
-			<div class="left">
-				Inloggen HIER (Placeholder)
-				<p>Email:<br>
-				<input type="text"><br>
-				Wachtwoord:<br>
-				<input type="text"></p>
-				<button type="button">Inloggen</button>
-			</div>
-			<div class="content">
-                            <p>Welkom op iDesign!<br></p><p><br>Op iDesign kun jij je eigen digitale portfolio uploaden
-                            (showcaseportfolio, ontwikkelingsportfolio en beoordelingsportfolio). Ook kun je berichten
-                            in het gastenboek van andere leden plaatsen (en je eigen gastenboek lezen).<br><br></p>
-                            <img src="img/homepc.png" alt="Flowers" style="max-width:100%;height:auto;">
-			</div>	
+		<div class="left">
+                    Inloggen HIER (Placeholder)
+                    <p>Email:<br>
+                    <input type="text"><br>
+                    Wachtwoord:<br>
+                    <input type="text"></p>
+                    <button type="button">Inloggen</button>
 		</div>
+		<div class="content">
+                    <p>Welkom op iDesign!<br></p><p><br>Op iDesign kun jij je eigen digitale portfolio uploaden
+                    (showcaseportfolio, ontwikkelingsportfolio en beoordelingsportfolio). Ook kun je berichten
+                    in het gastenboek van andere leden plaatsen (en je eigen gastenboek lezen).<br><br></p>
+                    <img src="img/homepc.png" alt="Flowers" style="max-width:100%;height:auto;">
+                </div>	
+            </div>
             <div id="footer">
                 <span class="menufooteritems">&#169; iDesign</span>
             </div>
