af42ce49aa009cbfe44ca5a83d1aed1fb526ab72
LAPTOP-DR460QUC\Ivar Huizing <ivar.huizing@student.stenden.com>
2018-01-23 11:37:56 +0100

    Protofolio map verwijderd en TitlePHP bijgewerkt.

--- TitlePHP.php
+++ TitlePHP.php
@@ -1,26 +1,27 @@
+<?php
+$titlephp= "Dit is een test php code";
+
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
-        <title>
-            <?php
+        <title> Title PHP</title>
+    </head>
+    <body>
+         <?php
             if (isset($_POST['submit'])) {
                 echo $_POST['Title'];
                 echo $_POST['Select'];
             }
             ?>
-        </title>
-    </head>
-    <body>
         <form method="post">
             <input type="text" name="Title">
-            <p><h2>OF Voorbeelden</h2></p>
-        
+            <h2>OF Voorbeelden</h2>
         <p><select name="Select">
             <option value="Portfolio">Portfolio</option>
             <option value="Port">Port</option>
             </select></p>
-        
-            <input type="submit" name="submit">
+        <input type="submit" name="submit">
         </form>
     </body>
 </html>
